5ce9d7b0159399a5d5165ac087347b5fc6a84f7f
mahdi31black <seyd.1372018077@gmail.com>
2025-05-07 19:26:53 +0330

    edit moxalafat

--- home/slider.js
+++ home/slider.js
@@ -1,4 +1,5 @@
 let currentIndex = 0;
+let autoSlideInterval;
 
 function moveSlide(direction) {
     const slides = document.querySelectorAll('.slide');
@@ -12,5 +13,34 @@ function moveSlide(direction) {
         currentIndex = 0;
     }
 
+    updateSlider();
+}
+
+function currentSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    if (index >= 1 && index <= totalSlides) {
+        currentIndex = index - 1;
+        updateSlider();
+        resetAutoSlide(); // ریست تایمر هنگام کلیک کاربر
+    }
+}
+
+function updateSlider() {
     const slider = document.querySelector('.slider');
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;}
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+        moveSlide(1); // هر 3.5 ثانیه به اسلاید بعدی برو
+    }, 3500);
+}
+
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval); // توقف تایمر فعلی
+    startAutoSlide(); // شروع مجدد تایمر
+}
+
+startAutoSlide();
