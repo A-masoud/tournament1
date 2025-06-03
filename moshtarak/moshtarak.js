f2b6fe58967dd4f6a5dc320a9ba9e99e00575e93
A-masoud <masoud.alipoor83.83@gmail.com>
2025-06-03 12:07:16 +0330

    همبرگر ساب منو ها در 500 پیکسل حل شد

--- moshtarak/moshtarak.js
+++ moshtarak/moshtarak.js
@@ -1,14 +1,24 @@
 const hamburger = document.getElementById('hamburger');
-const menu = document.querySelector('nav');  
+const menu = document.querySelector('nav');
 
 hamburger.addEventListener('click', () => {
-hamburger.classList.toggle('open');
-menu.classList.toggle('active');
+  hamburger.classList.toggle('open');
+  menu.classList.toggle('active');
 });
+
 document.querySelectorAll('.has-submenu > a').forEach(item => {
-item.addEventListener('click', function (e) {
-  e.preventDefault(); // جلوگیری از رفتن به لینک
-  const parent = this.parentElement;
-  parent.classList.toggle('open-submenu');
-});
+  item.addEventListener('click', function (e) {
+    e.preventDefault(); // جلوگیری از رفتن به لینک
+    const parent = this.parentElement;
+
+    // بستن همه‌ی ساب‌منوها به جز مورد کلیک‌شده
+    document.querySelectorAll('.has-submenu').forEach(el => {
+      if (el !== parent) {
+        el.classList.remove('open-submenu');
+      }
+    });
+
+    // باز یا بسته کردن ساب‌منوی مورد نظر
+    parent.classList.toggle('open-submenu');
+  });
 });
