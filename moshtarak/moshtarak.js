665a71c50d37ca618f895a4b1b9a04a1c0c9446a
A-masoud <masoud.alipoor83.83@gmail.com>
2025-06-05 21:36:21 +0330

    ورود کاربر با توکن

--- moshtarak/moshtarak.js
+++ moshtarak/moshtarak.js
@@ -1,3 +1,48 @@
+// moshtarak.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+// مقداردهی Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC5mI93Gj8Oo73OLFMdoRExN46Ffcr1AQ4",
+  authDomain: "tournify-app.firebaseapp.com",
+  projectId: "tournify-app",
+  storageBucket: "firenotify-app.appspot.com",
+  messagingSenderId: "273027702239",
+  appId: "1:273027702239:web:ae13272cba831cca2ef86a",
+  measurementId: "G-GEKS6X6RCV"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// پیدا کردن div مربوط به دکمه‌ها
+const userButtons = document.querySelector(".buttons");
+
+onAuthStateChanged(auth, (user) => {
+  if (user && userButtons) {
+    const username = localStorage.getItem("currentUsername") || "کاربر";
+
+    userButtons.innerHTML = `
+      <p> ${username} سلام خوش اومدی!</p>
+      <div>
+        <button id="logoutBtn">خروج </button>
+        <a href="../dashboard/dashboard.html">
+          <button>پنل کاربری </button>
+        </a>
+      </div>
+    `;
+
+    document.getElementById("logoutBtn").addEventListener("click", () => {
+      signOut(auth).then(() => {
+        localStorage.clear();
+        location.reload();
+      });
+    });
+  }
+});
+
+/////////////////////////////
 const hamburger = document.getElementById('hamburger');
 const menu = document.querySelector('nav');
 
@@ -22,3 +67,4 @@ document.querySelectorAll('.has-submenu > a').forEach(item => {
     parent.classList.toggle('open-submenu');
   });
 });
+
